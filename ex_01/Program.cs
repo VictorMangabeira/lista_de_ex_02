033544f08c1ca61f8f351aa55d813ac00c60c419
VICTOR DE OLIVEIRA MANGABEIRA <victor.omangabeira@senacsp.edu.br>
2025-01-23 17:12:39 -0300

    lista_02

---
+++ ex_01/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Ex_01
+ * Um funcionario deseja contar quantos dias de trabalho ele leva em um mês. Para isso, ele deve inserir os dias trabalhandos (de 1 a 31) até que digite 0, que indica que terminou de informa os dias. O Aluno deve implementar essa contagem utilizando as estruturas de repetição while, do while e for.
+ */
+
+/*int diasTrabalhados = 0;
+int dias;
+
+Console.WriteLine("Digite os dias trabalhados (0 para sair):");
+
+while (true)
+{
+    dias = Convert.ToInt32(Console.ReadLine());
+    if ( dias == 0 ) break;
+    diasTrabalhados++;
+
+}
+Console.WriteLine($"Total de dias trabalhados: {diasTrabalhados}");*/
+
+int diasTrabalhados = 0;
+int dias;
+
+do
+{
+    Console.WriteLine("Digite os dias trabalhados ( 0 para sair):");
+    dias = Convert.ToInt32(Console.ReadLine());
+    if (dias != 0 ) diasTrabalhados++;
+}
+while (dias != 0);
--- lista_exercicos_02/obj/Debug/net8.0/lista_exercicos_02.AssemblyInfo.cs
+++ lista_exercicos_02/obj/Debug/net8.0/lista_exercicos_02.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lista_exercicos_02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3afa1f4adf619346127892b5021903449cb7d160")]
 [assembly: System.Reflection.AssemblyProductAttribute("lista_exercicos_02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lista_exercicos_02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
